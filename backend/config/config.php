4856fc7677622feb686535720e91579cc9576ad2
wesleywil <wesleywilsonti@gmail.com>
2023-04-24 17:27:13 -0300

    added .env file

--- backend/config/config.php
+++ backend/config/config.php
@@ -5,8 +5,8 @@ error_reporting(E_ALL);
 // Database connection constants
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'product_list');
-define('DB_USER', 'wesley');
-define('DB_PASSWORD', '123Wes');
+define('DB_USER', $_ENV(DB_USER));
+define('DB_PASSWORD', $_ENV(DB_PASSWORD));
 
 
 // URL for the application - Will be changed
