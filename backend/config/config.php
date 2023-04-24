aa8f739c55decc5e768e4984463291d2f9cd046b
wesleywil <wesleywilsonti@gmail.com>
2023-04-24 14:21:30 -0300

    moved database configutation files to the right folder(config)

---
+++ backend/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+error_reporting(E_ALL);
+
+// Database connection constants
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'product_list');
+define('DB_USER', 'wesley');
+define('DB_PASSWORD', '123Wes');
+
+
+// URL for the application - Will be changed
+define('BASE_URL', 'http://localhost/myapp');
---
+++ backend/config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once(__DIR__ . '/config.php');
+
+//Connect to the database using PDO
+try {
+    $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4';
+    $pdo = new PDO($dsn, DB_USER, DB_PASSWORD);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    print_r($e);
+}
