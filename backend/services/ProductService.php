a6480463d64c1ec16ad8b98a7782c48135dfab96
wesleywil <wesleywilsonti@gmail.com>
2023-04-25 17:45:36 -0300

    updated ProductService.php need to re do everything

--- backend/services/ProductService.php
+++ backend/services/ProductService.php
@@ -10,11 +10,40 @@ class ProductService
         $this->db = $db;
     }
 
-    public function saveProduct(Product $product)
-    {
-        $stmt = $this->db->prepare("INSERT INTO products(sku, name, price, type) VALUES(?,?,?,?)");
-        $stmt->bind_param("ssds", $product->getSku(), $product->getName(), $product->getPrice(), $product->getType());
-        $stmt->execute();
-        $product->save($this->db);
-    }
+// public function saveProduct(Product $product)
+// {
+//     $sku = $product->getSku();
+//     $name = $product->getName();
+//     $price = $product->getPrice();
+//     $type = $product->getType();
+
+//     $stmt = $this->db->prepare("INSERT INTO products(sku, name, price, type) VALUES(?,?,?,?)");
+//     $stmt->bind_param("ssds", $sku, $name, $price, $type);
+//     $stmt->execute();
+//     $product->save($this->db);
+// }
+
+// public function getAllProducts()
+// {
+//     $stmt = $this->db->prepare("SELECT*FROM products");
+//     $stmt->execute();
+//     $result = $stmt->get_result();
+
+//     $products = array();
+
+//     while ($row = $result->fetch_assoc()) {
+//         $producType = $row['type'];
+//         $product = new $producType($row['sku'], $row['nanme'], $row['price'], ...$row);
+
+//         $product->setId($row['id']);
+//         $products[] = $product;
+//     }
+
+//     $output = '';
+//     foreach ($products as $product) {
+//         $output .= $product->display();
+//     }
+
+//     return $output;
+// }
 }
