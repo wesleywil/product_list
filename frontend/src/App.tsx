cadb20a49e99af0b0db211bc29146160db248d0b
wesleywil <wesleywilsonti@gmail.com>
2023-04-27 14:21:47 -0300

    added the pages to the App.tsx router, removed unsused css and fixed some small configurations

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,26 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "../node_modules/react-router-dom/dist/index";
+
+// Pages
+import Homepage from "./pages/homepage/homepage";
+import AddProduct from "./pages/add_product/add_product";
+
+// Components
+
+// Styles
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1 className="text-4xl text-red-500 font-bold">Homepage</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/addproduct" element={<AddProduct />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
