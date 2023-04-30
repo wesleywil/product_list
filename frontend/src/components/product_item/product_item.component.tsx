b9ee7bf7da32f9f520cf4dfb63f4ee52cb076b83
wesleywil <wesleywilsonti@gmail.com>
2023-04-30 12:16:59 -0300

    update style of product_item component

--- frontend/src/components/product_item/product_item.component.tsx
+++ frontend/src/components/product_item/product_item.component.tsx
@@ -6,15 +6,15 @@ interface ProductItemProp {
 
 const ProductItem = ({ item }: ProductItemProp) => {
   return (
-    <div className="border-2 p-2 w-52 h-42 rounded">
+    <div className="w-52 h-42 p-2  border-2 rounded">
       <div>
         <input type="checkbox" />
       </div>
       <div className="p-2 flex flex-col justify-center items-center text-xl">
         <h1>{item.sku}</h1>
-        <h2>{item.name}</h2>
+        <h2 className="text-center">{item.name}</h2>
         <h2>${item.price}</h2>
-        <div className="flex flex-col items-center text-center">
+        <div className="mt-2 flex flex-col items-center text-base text-center">
           <h2>{item.specificAttribute}</h2>
           <h2>{item.attributeValue}</h2>
         </div>
